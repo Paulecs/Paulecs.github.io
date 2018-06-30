b2a471e7392b2f49ddb2287ba7b0f14d8ffe1bfb
Agwu Paul Elechi <boypaule@yahoo.com>
2018-06-30 10:37:48 +0100

    Adding main.js

--- js/generators.js
+++
@@ -1,9 +0,0 @@
-function getEmployee(){
-	console.log('The function has started');
-	const names=['Paul','David', 'Peter', 'Ayodele', 'Kanu', 'Mercy', 'John'];
-		for(const name of names){
-			console.log(name);
-		}
-		console.log('The function has ended');
-}
-getEmployee();
