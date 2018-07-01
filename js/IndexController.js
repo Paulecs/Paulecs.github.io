4558676a1dc44db9b87b69b1e16693826ea2429f
Agwu Paul Elechi <boypaule@yahoo.com>
2018-07-01 01:01:52 +0100

    serviceworker and idb

--- js/IndexController.js
+++ js/IndexController.js
@@ -1,14 +1,45 @@
-if ('serviceWorker in navigator'){
-	navigator.serviceWorker.register('/serviceworker.js').then(function(registration){
-	console.log('ServiceWorker registration succeeded:', registration);
-	}).catch(function(error){
-		console.log('Service Worker registration failed', error);
-	});
-} else{
-		console.log('Service Workers not supported');
-	}
-	
-	if (!navigator.serviceWorker.controller) {
+import idb from 'idb';
+
+function openDatabase() {
+  // If the browser doesn't support service worker,
+  // we don't care about having a database
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+
+  return idb.open('currency', 1, function(upgradeDb) {
+    var store = upgradeDb.createObjectStore('currencies', {
+      keyPath: 'id'
+    });
+    store.createIndex('by-date', 'time');
+  });
+}
+
+export default function IndexController(container) {
+  this._container = container;
+  this._lostConnectionToast = null;
+  this._dbPromise = openDatabase();
+  this._registerServiceWorker();
+  this._cleanImageCache();
+
+  var indexController = this;
+
+  setInterval(function() {
+    indexController._cleanImageCache();
+  }, 1000 * 60 * 5);
+
+  this._showCachedMessages().then(function() {
+    indexController._openSocket();
+  });
+}
+
+IndexController.prototype._registerServiceWorker = function() {
+  if (!navigator.serviceWorker) return;
+
+  var indexController = this;
+
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    if (!navigator.serviceWorker.controller) {
       return;
     }
 
@@ -35,4 +66,140 @@ if ('serviceWorker in navigator'){
     window.location.reload();
     refreshing = true;
   });
+};
+
+IndexController.prototype._showCachedMessages = function() {
+  var indexController = this;
+
+  return this._dbPromise.then(function(db) {
+    // if we're already showing posts, eg shift-refresh
+    // or the very first load, there's no point fetching
+    // posts from IDB
+    if (!db || indexController._postsView.showingPosts()) return;
+
+    var index = db.transaction('currencyConveters')
+      .objectStore('currencyConverters').index('by-date');
+
+    return index.getAll().then(function(messages) {
+      indexController._postsView.addPosts(messages.reverse());
+    });
+  });
+};
+
+IndexController.prototype._trackInstalling = function(worker) {
+  var indexController = this;
+  worker.addEventListener('statechange', function() {
+    if (worker.state == 'installed') {
+      indexController._updateReady(worker);
+    }
+  });
+};
+
+IndexController.prototype._updateReady = function(worker) {
+  var toast = this._toastsView.show("New version available", {
+    buttons: ['refresh', 'dismiss']
+  });
+
+  toast.answer.then(function(answer) {
+    if (answer != 'refresh') return;
+    worker.postMessage({action: 'skipWaiting'});
+  });
+};
+
+// open a connection to the server for live updates
+IndexController.prototype._openSocket = function() {
+  var indexController = this;
+  var latestPostDate = this._postsView.getLatestPostDate();
+
+  // create a url pointing to /updates with the ws protocol
+  var socketUrl = new URL('/updates', window.location);
+  socketUrl.protocol = 'ws';
+
+  if (latestPostDate) {
+    socketUrl.search = 'since=' + latestPostDate.valueOf();
+  }
+
+  // this is a little hack for the settings page's tests,
+  // it isn't needed for Wittr
+  socketUrl.search += '&' + location.search.slice(1);
+
+  var ws = new WebSocket(socketUrl.href);
+
+  // add listeners
+  ws.addEventListener('open', function() {
+    if (indexController._lostConnectionToast) {
+      indexController._lostConnectionToast.hide();
+    }
+  });
+
+  ws.addEventListener('message', function(event) {
+    requestAnimationFrame(function() {
+      indexController._onSocketMessage(event.data);
+    });
+  });
+
+  ws.addEventListener('close', function() {
+    // tell the user
+    if (!indexController._lostConnectionToast) {
+      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
+    }
+
+    // try and reconnect in 5 seconds
+    setTimeout(function() {
+      indexController._openSocket();
+    }, 5000);
+  });
+};
+
+IndexController.prototype._cleanImageCache = function() {
+  return this._dbPromise.then(function(db) {
+    if (!db) return;
+
+    var imagesNeeded = [];
+
+    var tx = db.transaction('currencyConverters');
+    return tx.objectStore('currencyConverters').getAll().then(function(messages) {
+      messages.forEach(function(message) {
+        if (message.photo) {
+          imagesNeeded.push(message.photo);
+        }
+        imagesNeeded.push(message.avatar);
+      });
+
+      return caches.open('currencyConverter-content-imgs');
+    }).then(function(cache) {
+      return cache.keys().then(function(requests) {
+        requests.forEach(function(request) {
+          var url = new URL(request.url);
+          if (!imagesNeeded.includes(url.pathname)) cache.delete(request);
+        });
+      });
+    });
+  });
+};
+
+// called when the web socket sends message data
+IndexController.prototype._onSocketMessage = function(data) {
+  var messages = JSON.parse(data);
+
+  this._dbPromise.then(function(db) {
+    if (!db) return;
+
+    var tx = db.transaction('currencies', 'readwrite');
+    var store = tx.objectStore('currencies');
+    messages.forEach(function(message) {
+      store.put(message);
+    });
+
+    // limit store to 30 items
+    store.index('by-date').openCursor(null, "prev").then(function(cursor) {
+      return cursor.advance(30);
+    }).then(function deleteRest(cursor) {
+      if (!cursor) return;
+      cursor.delete();
+      return cursor.continue().then(deleteRest);
+    });
+  });
+
+  this._postsView.addPosts(messages);
 };
--- js/main.js
+++ js/main.js
@@ -1,18 +1,55 @@
-const currencies_url = 'https:/free.currencyconverterapi.com/api/v5/currencies';
-fetch(currencies_url).then(response =>{
-    if(response.staus!== 200){
-        console.log("App not responding correctly"+response.status);
-    return;
-    }
-    response.json().then(currencies =>{
-        console.log(currencies);
-        let currencyFrom = document.getElementById('currency-from');
-        let currencyTo = getElementById('currency-to');
-        for (const currency of currencies){
-            for (const id in currencies[currency]){
-                currencyFrom.innerHTML += `<option value="${currencies[currency][id].id}"> ${currencies[currency][id].id}`
-                currencyTo.innerHTML +=`<option value="${currencies[currency][id].id}"> ${currencies[currency][id].id}`
-            }
-        }
+let amount = document.querySelector("#amount");
+  	let fromCurrency = document.querySelector("#fromCurrency");
+  	let toCurrency = document.querySelector("#toCurrency");
+  	let btnConvert = document.querySelector('#convert');
+
+  	let url = 'https://free.currencyconverterapi.com/api/v5/currencies';
+
+  	 fetch( url,{ method : "GET"})
+			.then( function(response){ 
+				return response.json();
+			})
+			.then( function(results){
+				for(let result in results){
+                  for(let id in results[result]){
+                  	let optionA = `
+                      <option value="${results[result][id]['id']}">
+                         ${results[result][id]['currencyName']} (${results[result][id]['id']})
+                      </option>
+                  	`;
+                  	let optionB = `
+                      <option value="${results[result][id]['id']}">
+                         ${results[result][id]['currencyName']} (${results[result][id]['id']})
+                      </option>
+                  	`;
+                     fromCurrency.innerHTML += optionA;
+                     toCurrency.innerHTML += optionB;
+                 }
+               }
+			})
+			.catch( function(error){
+				console.log(error);
+			});
+
+function parseJSON(res){
+  return res.json()
+}		
+//   *****************************************//
+
+$("#convert").on("click", () => {
+  const amount1 = $('#amount').val();
+  const From = $('#fromCurrency option:selected').val();
+  const To = $('#toCurrency option:selected').val(); 
+  const query = `${From}_${To}`;
+  const queryUrl = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`; 
+  fetch(queryUrl)
+    .then(parseJSON)
+    .then(parsedData => {
+      for(let rate in parsedData){
+         let calc = (parsedData[rate].val); 
+      let total = (Number(amount1) * calc);
+      $('#output').val(Math.round(total * 100) / 100);
+        
+      }
     })
-})
+});
